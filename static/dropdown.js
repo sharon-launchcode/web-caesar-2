5a0f9f833ed2d8a15f3e9820419bb9492ea51e07
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-15 08:19:34 -0500

    adding functionality to bootstrap

--- static/dropdown.js
+++ static/dropdown.js
@@ -17,3 +17,22 @@
           }
         }
       }
+
+    function bootstrapDropdown() {
+        document.getElementById("bootstrapDropdownButton").classList.toggle("show");
+    }
+    
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+    
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
