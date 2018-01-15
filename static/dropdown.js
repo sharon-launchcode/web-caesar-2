f5b629d1c733ff500dd6593b9a9cb7246498e9fb
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-15 09:13:54 -0500

    working on navbar

--- static/dropdown.js
+++ static/dropdown.js
@@ -36,3 +36,39 @@
         }
       }
     }
+  function testDropdown() {
+      document.getElementById("testDropdownButton").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+  
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function testDropdown2() {
+    document.getElementById("testDropdownButton2").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
